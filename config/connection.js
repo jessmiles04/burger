51900196dca1f174d009d1e89663905282b3491f
Jessica Miles <Jessica Miles>
2017-05-05 20:14:03 -0400

    config file begins

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+//mysql connection dependencies
+var mysql = require('mysql');
+
+//actual connection variable
+var connection;
+
+//connection on localhost
+connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: 'D@zy2012',
+    database: 'burgers_db'
+})
+
+connection.connect(function(err){
+    if (err) {
+        //message if there is an error connecting with MySQL
+        console.error('Error: Connection issue with --' + err.stack + '\n\n');
+    }
+    console.log('Connection to db')
+});
+
