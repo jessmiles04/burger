7119e149de63f4b43fb58ee9300b7a99b0dd40b5
Jessica Miles <Jessica Miles>
2017-05-05 21:15:34 -0400

    working on controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,23 @@
 //import express
+var express = require('express');
 //import burger.js
+var burger = require('../models/burger.js');
 //create router for app
+var router = express.Router();
+
+
+//Actual routes
+//Get
+router.get('/', function(req, res){
+//select all
+})
+
+router.post('/burgers', function(req, res){
+    //insert one
+})
+
+router.put('/burgers/:id', function(req, res){
+    //update one
+})
 //export router
+module.exports = router;
